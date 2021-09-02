39b7d8ff3a06b7435003b38446b0ecb272c15bd9
Michael I Chen <michael.chen.0@gmail.com>
2021-09-01 23:24:15 -0700

    Fix .gitignores and BAUM names

---
+++ datasets/BAUM1/BAUM1_data_files.py
@@ -0,0 +1,118 @@
+"""
+This script writes the file paths, label, and other metadata of records to retain for model development in a tab-delimited csv.
+The resultant tsv is used to help aggregate all records into a unified dataset.
+Records are from BAUM-1.
+"""
+
+from os.path import exists
+
+# I copied and pasted from Annotations_BAUM1a.xlsx and Annotations_BAUM1s.xlsx to make preclean.tsv with a newline between the subsets
+with open("preclean.tsv", "r") as f:
+    lines = f.readlines()
+
+codemaker = lambda x: {k: v for k, v in zip([str(_) for _ in range(1, len(x) + 1)], x)}
+# codes in the acted and spontaneous sets
+CODES_ACT, CODES_SPO = codemaker(
+    ["ang", "bor", "dis", "fea", "hap", "int", "sad", "sur", "uns"]
+), codemaker(
+    [
+        "ang",
+        "bor",
+        "bot",
+        "con",  # contempt
+        "con",  # concentrating
+        "dis",
+        "fea",
+        "hap",
+        "neu",
+        "sad",
+        "sur",
+        "thi",
+        "uns",
+    ]
+)
+
+# I set the 266th line in preclean.tsv to a single newline
+acted = lines[:265]
+spont = lines[266:]
+
+KEEP_EMOS = [
+    "ang",
+    "bor",
+    "bot",
+    "con",
+    "dis",
+    "fea",
+    "hap",
+    "int",
+    "neu",
+    "sad",
+    "sur",
+    "uns",
+]
+VALENCE = {k: "-1" for k in KEEP_EMOS}
+VALENCE["neu"] = "0"
+VALENCE["hap"], VALENCE["int"] = "1", "1"
+
+LANG = "tur"  # ISO 639-3 Turkish
+LANG2 = "tr"  # ISO 639-1 Turkish
+out = []
+
+for record in acted:
+    file, emo, code, gender = record.rstrip().lower().split("\t")
+    emo = emo[:3]
+    if CODES_ACT[code] != emo:
+        print("uh oh, emo code does not match label in annotations", file)
+    elif emo in KEEP_EMOS:
+        speaker = file.lower().split("_", 1)[0]
+        file = "\\".join(("BAUM1a_MP4_all", speaker, ".".join((file, "mp4"))))
+        out.append(
+            "\t".join(
+                (
+                    file,
+                    # retroactive fix: change "uns" to "unc"
+                    "unc" if emo == "uns" else emo,
+                    VALENCE[emo],
+                    LANG,
+                    LANG2,
+                    gender,
+                    "BAUM1\n",
+                )
+            )
+        )
+
+for record in spont:
+    file, emo, code, gender = record.rstrip().lower().split("\t")
+    # disambiguate contempt and concentrating
+    if emo == "Concentrating" and code != "4":
+        print("uh oh, emo code does not match label in annotations", file)
+    emo = emo[:3].lower()
+    if CODES_SPO[code] != emo:
+        print("uh oh, emo code does not match label in annotations", file)
+        # S015_007 was flagged and manually corrected in preclean.tsv
+        # it was ambiguous whether it was happy or neutral in the annotation file (spontaneous)
+        # it was pretty obvious to me that it was a happy sample (smiling and laughing)
+    elif emo in KEEP_EMOS:
+        speaker = file.lower().split("_", 1)[0]
+        file = "\\".join(("BAUM1s_MP4 - All", speaker, ".".join((file, "mp4"))))
+        if not exists(file):
+            # This never happens since all files exist
+            print("uh oh, %s doesn't exist!" % file)
+        out.append(
+            "\t".join(
+                (
+                    file,
+                    # retroactive fix: change "uns" to "unc"
+                    "unc" if emo == "uns" else emo,
+                    VALENCE[emo],
+                    LANG,
+                    LANG2,
+                    gender,
+                    "BAUM1\n",
+                )
+            )
+        )
+
+with open("BAUM1_data_files.tsv", "w") as f:
+    [f.write(record) for record in out]
+print("done")
---
+++ datasets/EmoReact_V_1.0/EmoReact_V_1.0_data_files.py
@@ -0,0 +1,115 @@
+"""
+This script writes the file paths, label, and other metadata of records to retain for model development in a tab-delimited csv.
+The resultant tsv is used to help aggregate all records into a unified dataset.
+Records are from the EmoReact dataset.
+"""
+
+from collections import namedtuple
+
+LANG = "eng"  # ISO 639-3 English
+LANG2 = "en"  # ISO 639-1 English
+
+# Speaker is adult interviewer, not child subject
+manually_omit = {"VCR107_2.mp4", "BULLYING27_2.mp4", "GAMEBOY19_2.mp4"}
+
+rating_vector = namedtuple(
+    "RatingVector",
+    [
+        "curiosity",
+        "uncertainty",
+        "excitement",
+        "happiness",
+        "surprise",
+        "disgust",
+        "fear",
+        "frustration",
+    ],
+)
+
+
+def fuzzy_emotion_vote(ratings) -> tuple[str, str]:
+    """Return a fuzzy emotion vote from a ratings vector."""
+
+    emotions = {
+        "cur": ratings.curiosity,
+        "exc": ratings.excitement,
+        "hap": ratings.happiness,
+        "unc": ratings.uncertainty,
+        "sur": ratings.surprise,
+        "dis": ratings.disgust,
+        "fea": ratings.fear,
+        "fru": ratings.frustration,
+    }
+
+    any_negative = any(  # negative emotions
+        {
+            emotions["unc"],
+            emotions["sur"],
+            emotions["dis"],
+            emotions["fea"],
+            emotions["fru"],
+        }
+    )
+
+    if any({emotions["cur"], emotions["exc"], emotions["hap"]}):  # positive emotions
+        if any_negative:
+            valence = ""
+        else:
+            valence = "1"
+    else:
+        if any_negative:
+            valence = "-1"
+        else:
+            # all ratings are 0
+            return "neu", "0"
+
+    if sum(ratings) > 1:
+        emotion = "unk"
+    else:
+        for key in emotions.keys():
+            if emotions[key]:
+                emotion = key
+                break
+
+    return emotion, valence
+
+
+valence_from_float = lambda x: "-1" if x < 4 else "1" if x > 4 else "0"
+strip_split = lambda x, c: x.strip().split(c)
+
+with open("EmoReact_V_1.0_data_files.tsv", "w") as f1:
+    for split in {"train", "val", "test"}:
+        # The *2.txt files include manual speaker gender annotations
+        with open(f"{split}_names2.txt", "r") as f2:
+            with open(f"Labels/{split}_labels.text", "r") as f3:
+                for filename_gender, votes in zip(f2, f3):
+                    # manual annotations: filename*gender
+                    file, gender = strip_split(filename_gender, "*")
+                    ratings = [float(_) for _ in strip_split(votes, ",")]
+                    perceived_valence = valence_from_float(ratings.pop(-1))
+                    (
+                        perceived_emotion,
+                        valence_of_perceived_emotion,
+                    ) = fuzzy_emotion_vote(rating_vector(*ratings))
+                    keep = False
+                    if perceived_emotion == "unk":
+                        keep = bool(int(perceived_valence))
+                    elif valence_of_perceived_emotion == perceived_valence:
+                        keep = True
+                    if file in manually_omit:
+                        keep = False
+                    if keep:
+                        f1.write(
+                            "\t".join(
+                                [
+                                    f"Data/{split}/{file}",
+                                    perceived_emotion,
+                                    perceived_valence,
+                                    LANG,
+                                    LANG2,
+                                    gender,
+                                    "EmoReact_V_1.0\n",
+                                ]
+                            )
+                        )
+print("done")
