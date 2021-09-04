1f0dfb9c1f4948487a2355da86174ba83e81c51a
Michael I Chen <michael.chen.0@gmail.com>
2021-09-04 00:15:48 -0700

    Add data organization files for EYASE

---
+++ datasets/EYASE/EYASE_data_files.py
@@ -0,0 +1,35 @@
+"""
+This script writes the file paths, label, and other metadata of records to retain for model development in a tab-delimited csv.
+The resultant tsv is used to help aggregate all records into a unified dataset.
+Records are from the Egyptian Arabic speech emotion database (EYASE).
+"""
+
+from os import walk
+
+LANG = "arz"  # ISO 639-3 Egyptian Arabic
+LANG2 = "ar-eg"  # ISO 639-1 Arabic + ISO 3166-1 Egypt
+
+valence = dict.fromkeys(["ang", "sad"], "-1")
+valence["hap"], valence["neu"] = "1", "0"
+
+with open("EYASE_data_files.tsv", "w") as f:
+    for gender in {"Female", "Male"}:
+        for number in {"01", "02", "03"}:
+            folder = f"{gender}{number}"
+            for _, _, files in walk(folder):
+                for file_ in files:
+                    emo = file_.split()[0].split("_")[-1]
+                    f.write(
+                        "\t".join(
+                            [
+                                "/".join([folder, file_]),
+                                emo,
+                                valence[emo],
+                                LANG,
+                                LANG2,
+                                gender[0].lower(),
+                                "esd\n",
+                            ]
+                        )
+                    )
+print("done")
